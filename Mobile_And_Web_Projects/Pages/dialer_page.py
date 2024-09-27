fee2554744d951be232ca8830c0a44715ca56ac2
dorinste <dorin.stein595@gmail.com>
2024-09-27 17:06:48 +0300

    first commit

--- Mobile_And_Web_Projects/Pages/dialer_page.py
+++ Mobile_And_Web_Projects/Pages/dialer_page.py
@@ -6,10 +6,7 @@ class dialer_page():
         self.mobile_driver = mobile_driver
         self.dialer_locator = 'com.google.android.apps.nexuslauncher:id/overview_actions_view'
         self.key_pad_locator = 'com.google.android.dialer:id/dialpad_fab'
-        dialer = self.mobile_driver.find_element(by=AppiumBy.ID, value=self.dialer_locator)
-        dialer.click()
-        key_pad = self.mobile_driver.find_element(by=AppiumBy.ID, value=self.key_pad_locator)
-        key_pad.click()
+
         num1 = self.mobile_driver.find_element(by=AppiumBy.ID, value='com.google.android.dialer:id/one')
         num2 = self.mobile_driver.find_element(by=AppiumBy.ID, value='com.google.android.dialer:id/two')
         num3 = self.mobile_driver.find_element(by=AppiumBy.ID, value='com.google.android.dialer:id/three')
@@ -23,12 +20,6 @@ class dialer_page():
 
         self.call_button_locator = 'com.google.android.dialer:id/dialpad_voice_call_button'
 
-    # def click_on_key_pad(self):
-    #     dialer = self.mobile_driver.find_element(by=AppiumBy.ID, value=self.dialer_locator)
-    #     dialer.click()
-    #
-    #     key_pad = self.mobile_driver.find_element(by=AppiumBy.ID, value=self.key_pad_locator)
-    #     key_pad.click()
 
     def dial_number(self, num_to_dial):
         print(f'dialing to number', num_to_dial)
