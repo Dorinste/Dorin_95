57122f55b7d4ff36c5dab0938edddd7a62fdf410
dorinste <dorin.stein595@gmail.com>
2024-09-28 09:56:25 +0300

    first commit

---
+++ Mobile_And_Web_Projects/Pages/locators.py
@@ -0,0 +1,10 @@
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.by import By
+
+
+
+class HelpCenterLocators(object):
+    Way_To_Connect =(By.CLASS_NAME, 'ups-analytics ups-cta ups-cta-secondary')
+
+class DialerPageLocators(object):
+    Dialer = (AppiumBy.ID,'com.google.android.apps.nexuslauncher:id/overview_actions_view')
